ec1d23341ee3dc4b87d789999849a7361eebbca5
ajaks <ajaks.spb@gmail.com>
2020-04-29 22:32:26 +0300

    Add `post` method.

--- main.py
+++ main.py
@@ -19,12 +19,41 @@ async def posts(request):
         if item['deleted'] is False:
             if datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%S') < datetime.now():
                 item['comments_count'] = len([x for x in comments_data['comments'] if x['post_id'] == item['id']])
+                del item['deleted']
                 result.append(item)
 
     return web.json_response({
-            'posts': sorted(result, key=lambda r: r['date'], reverse=True),
+            'posts': sorted(result, key=lambda r: datetime.strptime(r['date'], '%Y-%m-%dT%H:%M:%S'), reverse=True),
             'posts_count': len(result)
     })
+
+
+@routes.get('/post/{id:\d+}')
+async def post(request):
+    post_id = int(request.match_info['id'])
+
+    with open('data/post.json') as f:
+        posts_data = json.load(f)
+
+    with open('data/comments.json') as f:
+        comments_data = json.load(f)
+
+    result = None
+
+    for item in posts_data['posts']:
+        if item['id'] == post_id and datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%S') < datetime.now():
+            result = item
+
+    if not result or result['deleted']:
+        raise web.HTTPNotFound()
+
+    comments = [x for x in comments_data['comments'] if x['post_id'] == result['id']]
+
+    result['comments'] = sorted(comments, key=lambda r: datetime.strptime(r['date'], '%Y-%m-%dT%H:%M:%S'), reverse=True)
+    result['comments_count'] = len(comments)
+    del result['deleted']
+
+    return web.json_response(result)
 
 
 app = web.Application()
